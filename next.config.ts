732c642899206e17a465302e67b9b4709889323e
Mab Rehab <mutiaraabadibinjai@gmail.com>
2026-02-28 20:01:28 +0700

    Prepare for Vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",            // untuk generate static HTML
-  images: {
-    unoptimized: true,         // matikan Image Optimization API
-  },
+  output: "export",          // agar build statik
+  images: { unoptimized: true }, // next/image bisa pakai tanpa server
+  assetPrefix: "./",          // penting agar CSS/JS jalan di GitHub Pages
 };
 
 export default nextConfig;
